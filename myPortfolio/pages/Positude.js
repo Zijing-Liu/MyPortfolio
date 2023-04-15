589557ad56d82749aad8f3a2de720c8eedc9560b
Celine Liu <zijingliu2021@outlook.com>
2023-04-15 15:06:43 -0700

    added js view larger images

--- myPortfolio/pages/Positude.js
+++ myPortfolio/pages/Positude.js
@@ -1,1 +1,18 @@
+// JS function to view large picutre in a modal when users cilcks on the images in the prototye section
+function zoomIn() {
+  var modal = document.getElementById("modal");
 
+  var imgs = document.querySelectorAll("img");
+  var modalImg = document.getElementById("modal-img");
+
+  imgs.forEach((i) => {
+    i.onclick = function () {
+      modalImg.src = i.src;
+      modal.style.display = "block";
+    };
+  });
+
+  modalImg.onclick = function () {
+    modal.style.display = "none";
+  };
+}
