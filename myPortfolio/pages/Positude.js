bb7fa2dce02176fcecb4ed8189c463c394a71426
Celine Liu <zijingliu2021@outlook.com>
2023-04-20 11:45:10 -0700

    responsive design changes

--- myPortfolio/pages/Positude.js
+++ myPortfolio/pages/Positude.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const ham = document.getElementById("hambuger");
+  let dropdown = document.getElementById("dropDown");
+
+  ham.addEventListener("click", () => {
+    if (dropdown.style.display === "block") {
+      dropdown.style.display = "none";
+    } else {
+      dropdown.style.display = "block";
+    }
+  });
+});
+
 // JS function to view large picutre in a modal when users cilcks on the images in the prototye section
 function zoomIn() {
   var modal = document.getElementById("modal");
@@ -16,24 +29,25 @@ function zoomIn() {
     modal.style.display = "none";
   };
 }
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA1Zd2fc1cWOwZ_WiQs9pFdU6Y0XKSwpxo",
-  authDomain: "nodeapp-1801a.firebaseapp.com",
-  projectId: "nodeapp-1801a",
-  storageBucket: "nodeapp-1801a.appspot.com",
-  messagingSenderId: "1028228763263",
-  appId: "1:1028228763263:web:2a952ff875573f30a8f94a",
-  measurementId: "G-H9HE39LZH8",
-};
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA1Zd2fc1cWOwZ_WiQs9pFdU6Y0XKSwpxo",
+//   authDomain: "nodeapp-1801a.firebaseapp.com",
+//   projectId: "nodeapp-1801a",
+//   storageBucket: "nodeapp-1801a.appspot.com",
+//   messagingSenderId: "1028228763263",
+//   appId: "1:1028228763263:web:2a952ff875573f30a8f94a",
+//   measurementId: "G-H9HE39LZH8",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
